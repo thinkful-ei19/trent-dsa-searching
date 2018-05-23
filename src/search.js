c964584e4a2a24f49a87f2e171fec8b6553b385e
trentduncan <trent.dunc@gmail.com>
2018-05-23 11:15:54 -0700

    wrote step by step instructions of how to look up a book in the dewey decimal system

---
+++ src/search.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// The Dewey Decimal system
+// Since the books are already in order of decimal number, first step would be to cut the dataset in half and
+// check if the number you are looking for is greater or less than the middle. Based on that you can reduce
+// the dataset by half and then you can loop through the remaining side until you find the correct decimal, and then
+// you check it to the title for each of those until you return the book
+
