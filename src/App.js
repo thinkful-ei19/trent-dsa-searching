726cee38ed3f64a0fa24e12f922df833b577e55b
trentduncan <trent.dunc@gmail.com>
2018-05-23 09:40:39 -0700

    implemented binary search function

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <InputField title={'Linear Search'} name={'linear-search'}/><br/>
-        <InputField title={'Binary Search'} name={'binary-search'}/>
+        <InputField title={'Linear Search'} name={'linear-search'} isBinary={false}/><br/>
+        <InputField title={'Binary Search'} name={'binary-search'} isBinary={true}/>
       </div>
     );
   }
--- src/components/input-field.js
+++ src/components/input-field.js
@@ -28,21 +28,40 @@ export default class InputField extends React.Component {
     });
   }
 
-  binarySearch(arr,value, start, end) {
-
+  binarySearch(arr, value, start=0, end= arr.length-1, operations = 0) {
+    let displayMsg = null;
+    operations++;
+    if (start > end) {
+      displayMsg = 'Number not in the Dataset'; 
+    }
+    const index = Math.floor((start + end)/2);
+    if (arr[index] === value) {
+      displayMsg = index;
+    } 
+    if (displayMsg) {
+      this.setState({
+        displayMsg,
+        operations
+      })
+      return;
+    }
+    if (arr[index] < value) {
+      return this.binarySearch(arr, value, index + 1, end, operations);
+    }
+    if (arr[index] > value) {
+      return this.binarySearch(arr, value, start, index - 1, operations);
+    }
+    
   }
-
-//   binarySearch() {
-
-//   }
 
   render () {
     const array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]; 
+    const sortedArray = array.sort((a,b) => a-b);
     return (
         <form onSubmit={event => {
             event.preventDefault();
             const inputValue = Number(event.target.search.value);
-            this.linearSearch(array, inputValue);
+            this.props.isBinary ? this.binarySearch(sortedArray, inputValue) : this.linearSearch(array, inputValue);
             event.target.search.value = '';
         }}>
             <label htmlFor={this.props.name}>{this.props.title}</label><br/>
